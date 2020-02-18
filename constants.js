98262dce360eed86d1ea2be3ab23da0930c9a07f
Jonatas Cavalcanti Pereira <jonatas.pereira@pitang.com>
2020-02-17 22:55:58 -0300

    alterando campanha para fanta uva

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const constants = {
     PARTNER_1: 'Coca-Cola',
-    CAMPAIGN_1: 'Fanta laranja e guaraná',
-    ITEM_1_CAMPAIGN_1: 'Fanta laranja',
+    CAMPAIGN_1: 'Fanta uva e guaraná',
+    ITEM_1_CAMPAIGN_1: 'Fanta uva',
     ITEM_2_CAMPAIGN_1: 'Fanta guaraná',
     PARTNER_2: 'Pizza Hut',
     CAMPAIGN_2: 'Pizza quadrada ou redonda',
